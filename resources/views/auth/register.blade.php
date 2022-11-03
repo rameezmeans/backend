27a71749350af7feeaaff2d037a97ea232202eac
Rameez Bhatti <xrkalix@gmail.com>
2022-11-03 06:51:14 +0500

    pages theme added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,81 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+    <meta charset="utf-8" />
+    <title>Pages - Admin Dashboard UI Kit - Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no" />
+    <link rel="apple-touch-icon" href="pages/ico/60.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="pages/ico/76.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="pages/ico/120.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="pages/ico/152.png">
+    <link rel="icon" type="image/x-icon" href="favicon.ico" />
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-touch-fullscreen" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <meta content="" name="description" />
+    <meta content="" name="author" />
+    <link href="assets/plugins/pace/pace-theme-flash.css" rel="stylesheet" type="text/css" />
+    <link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/plugins/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css" />
+    <link href="assets/plugins/jquery-scrollbar/jquery.scrollbar.css" rel="stylesheet" type="text/css" media="screen" />
+    <link href="assets/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" media="screen" />
+    <link href="assets/plugins/switchery/css/switchery.min.css" rel="stylesheet" type="text/css" media="screen" />
+    <link href="assets/plugins/nvd3/nv.d3.min.css" rel="stylesheet" type="text/css" media="screen" />
+    <link href="assets/plugins/mapplic/css/mapplic.css" rel="stylesheet" type="text/css" />
+    <link href="assets/plugins/rickshaw/rickshaw.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/plugins/bootstrap-datepicker/css/datepicker3.css" rel="stylesheet" type="text/css" media="screen">
+    <link href="assets/plugins/jquery-metrojs/MetroJs.css" rel="stylesheet" type="text/css" media="screen" />
+    <link href="pages/css/pages-icons.css" rel="stylesheet" type="text/css">
+    <link class="main-stylesheet" href="pages/css/pages.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body class="fixed-header dashboard">
+  
+    @yield('content')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+   <!-- BEGIN VENDOR JS -->
+   <script src="assets/plugins/pace/pace.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/jquery/jquery-3.2.1.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/modernizr.custom.js" type="text/javascript"></script>
+   <script src="assets/plugins/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/popper/umd/popper.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/jquery/jquery-easy.js" type="text/javascript"></script>
+   <script src="assets/plugins/jquery-unveil/jquery.unveil.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/jquery-ios-list/jquery.ioslist.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/jquery-actual/jquery.actual.min.js"></script>
+   <script src="assets/plugins/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+   <script type="text/javascript" src="assets/plugins/select2/js/select2.full.min.js"></script>
+   <script type="text/javascript" src="assets/plugins/classie/classie.js"></script>
+   <script src="assets/plugins/switchery/js/switchery.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/nvd3/lib/d3.v3.js" type="text/javascript"></script>
+   <script src="assets/plugins/nvd3/nv.d3.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/nvd3/src/utils.js" type="text/javascript"></script>
+   <script src="assets/plugins/nvd3/src/tooltip.js" type="text/javascript"></script>
+   <script src="assets/plugins/nvd3/src/interactiveLayer.js" type="text/javascript"></script>
+   <script src="assets/plugins/nvd3/src/models/axis.js" type="text/javascript"></script>
+   <script src="assets/plugins/nvd3/src/models/line.js" type="text/javascript"></script>
+   <script src="assets/plugins/nvd3/src/models/lineWithFocusChart.js" type="text/javascript"></script>
+   <script src="assets/plugins/mapplic/js/hammer.min.js"></script>
+   <script src="assets/plugins/mapplic/js/jquery.mousewheel.js"></script>
+   <script src="assets/plugins/mapplic/js/mapplic.js"></script>
+   <script src="assets/plugins/rickshaw/rickshaw.min.js"></script>
+   <script src="assets/plugins/jquery-metrojs/MetroJs.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/jquery-sparkline/jquery.sparkline.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/skycons/skycons.js" type="text/javascript"></script>
+   <script src="assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js" type="text/javascript"></script>
+   <!-- END VENDOR JS -->
+   <!-- BEGIN CORE TEMPLATE JS -->
+   <!-- BEGIN CORE TEMPLATE JS -->
+   <script src="pages/js/pages.js"></script>
+   <!-- END CORE TEMPLATE JS -->
+   <!-- BEGIN PAGE LEVEL JS -->
+   <script src="assets/js/scripts.js" type="text/javascript"></script>
+   <!-- END PAGE LEVEL JS -->
+   <!-- END CORE TEMPLATE JS -->
+   <!-- BEGIN PAGE LEVEL JS -->
+   <script src="assets/js/dashboard.js" type="text/javascript"></script>
+   <script src="assets/js/scripts.js" type="text/javascript"></script>
+   <!-- END PAGE LEVEL JS -->
+ </body>
+</html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,81 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+    <meta charset="utf-8" />
+    <title>Pages - Admin Dashboard UI Kit - Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no" />
+    <link rel="apple-touch-icon" href="pages/ico/60.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="pages/ico/76.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="pages/ico/120.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="pages/ico/152.png">
+    <link rel="icon" type="image/x-icon" href="favicon.ico" />
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-touch-fullscreen" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <meta content="" name="description" />
+    <meta content="" name="author" />
+    <link href="assets/plugins/pace/pace-theme-flash.css" rel="stylesheet" type="text/css" />
+    <link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/plugins/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css" />
+    <link href="assets/plugins/jquery-scrollbar/jquery.scrollbar.css" rel="stylesheet" type="text/css" media="screen" />
+    <link href="assets/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" media="screen" />
+    <link href="assets/plugins/switchery/css/switchery.min.css" rel="stylesheet" type="text/css" media="screen" />
+    <link href="assets/plugins/nvd3/nv.d3.min.css" rel="stylesheet" type="text/css" media="screen" />
+    <link href="assets/plugins/mapplic/css/mapplic.css" rel="stylesheet" type="text/css" />
+    <link href="assets/plugins/rickshaw/rickshaw.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/plugins/bootstrap-datepicker/css/datepicker3.css" rel="stylesheet" type="text/css" media="screen">
+    <link href="assets/plugins/jquery-metrojs/MetroJs.css" rel="stylesheet" type="text/css" media="screen" />
+    <link href="pages/css/pages-icons.css" rel="stylesheet" type="text/css">
+    <link class="main-stylesheet" href="pages/css/pages.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body class="fixed-header dashboard">
+  
+   @yield('content')
+   
+   <!-- BEGIN VENDOR JS -->
+   <script src="assets/plugins/pace/pace.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/jquery/jquery-3.2.1.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/modernizr.custom.js" type="text/javascript"></script>
+   <script src="assets/plugins/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/popper/umd/popper.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/jquery/jquery-easy.js" type="text/javascript"></script>
+   <script src="assets/plugins/jquery-unveil/jquery.unveil.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/jquery-ios-list/jquery.ioslist.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/jquery-actual/jquery.actual.min.js"></script>
+   <script src="assets/plugins/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+   <script type="text/javascript" src="assets/plugins/select2/js/select2.full.min.js"></script>
+   <script type="text/javascript" src="assets/plugins/classie/classie.js"></script>
+   <script src="assets/plugins/switchery/js/switchery.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/nvd3/lib/d3.v3.js" type="text/javascript"></script>
+   <script src="assets/plugins/nvd3/nv.d3.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/nvd3/src/utils.js" type="text/javascript"></script>
+   <script src="assets/plugins/nvd3/src/tooltip.js" type="text/javascript"></script>
+   <script src="assets/plugins/nvd3/src/interactiveLayer.js" type="text/javascript"></script>
+   <script src="assets/plugins/nvd3/src/models/axis.js" type="text/javascript"></script>
+   <script src="assets/plugins/nvd3/src/models/line.js" type="text/javascript"></script>
+   <script src="assets/plugins/nvd3/src/models/lineWithFocusChart.js" type="text/javascript"></script>
+   <script src="assets/plugins/mapplic/js/hammer.min.js"></script>
+   <script src="assets/plugins/mapplic/js/jquery.mousewheel.js"></script>
+   <script src="assets/plugins/mapplic/js/mapplic.js"></script>
+   <script src="assets/plugins/rickshaw/rickshaw.min.js"></script>
+   <script src="assets/plugins/jquery-metrojs/MetroJs.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/jquery-sparkline/jquery.sparkline.min.js" type="text/javascript"></script>
+   <script src="assets/plugins/skycons/skycons.js" type="text/javascript"></script>
+   <script src="assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js" type="text/javascript"></script>
+   <!-- END VENDOR JS -->
+   <!-- BEGIN CORE TEMPLATE JS -->
+   <!-- BEGIN CORE TEMPLATE JS -->
+   <script src="pages/js/pages.js"></script>
+   <!-- END CORE TEMPLATE JS -->
+   <!-- BEGIN PAGE LEVEL JS -->
+   <script src="assets/js/scripts.js" type="text/javascript"></script>
+   <!-- END PAGE LEVEL JS -->
+   <!-- END CORE TEMPLATE JS -->
+   <!-- BEGIN PAGE LEVEL JS -->
+   <script src="assets/js/dashboard.js" type="text/javascript"></script>
+   <script src="assets/js/scripts.js" type="text/javascript"></script>
+   <!-- END PAGE LEVEL JS -->
+ </body>
 </html>
