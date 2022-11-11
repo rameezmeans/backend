b73fe3e0f1853eccc37f9e5fcd7d5f12c957973d
Rameez Bhatti <xrkalix@gmail.com>
2022-11-11 22:46:33 +0500

    file module until engineer

--- resources/views/files/files.blade.php
+++ resources/views/files/files.blade.php
@@ -5,7 +5,7 @@
     <!-- START PAGE CONTENT -->
     <div class="content sm-gutter">
       <!-- START CONTAINER FLUID -->
-        <div class=" container-fluid   container-fixed-lg bg-white">
+        <div class="container-fluid bg-white">
           <div class="card card-transparent m-t-40">
             <div class="card-header ">
                 <div class="card-title"><h3>Files</h3>
@@ -18,21 +18,50 @@
                     <table class="table table-hover demo-table-search table-responsive-block dataTable no-footer" id="tableWithSearch" role="grid" aria-describedby="tableWithSearch_info">
                         <thead>
                             <tr role="row">
-                                <th class="" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Title: activate to sort column descending" style="width: 250px;">Name</th>
-                                <th class="sorting" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-label="Activities: activate to sort column ascending" style="width: 42px;">Status</th>
-                                <th class="sorting" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-label="Activities: activate to sort column ascending" style="width: 42px;">Credits</th>
+                                <th class="" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Title: activate to sort column descending" style="width: 20px;">Task ID</th>
                                 <th class="sorting" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-label="Status: activate to sort column ascending" style="width: 150px;">Customer</th>
-                                <th class="sorting" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-label="Last Update: activate to sort column ascending" style="width: 100px;">Date Created</th>
+                                <th class="" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Title: activate to sort column descending" style="width: 150px;">Vehicle</th>
+                                <th class="sorting" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-label="Activities: activate to sort column ascending" style="width: 42px;">ECU Type</th>
+                                <th class="sorting" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-label="Activities: activate to sort column ascending" style="width: 42px;">Status</th>
+                                <th class="sorting" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-label="Activities: activate to sort column ascending" style="width: 150px;">Stages</th>
+                                <th class="sorting" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-label="Activities: activate to sort column ascending" style="width: 650px;">Options</th>
+                                <th class="sorting" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-label="Activities: activate to sort column ascending" style="width: 42px;">Credits</th>
+                                <th class="sorting" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-label="Last Update: activate to sort column ascending" style="width: 100px;">Date Uploaded</th>
                             </tr>
                         </thead>
                         <tbody>
                           @foreach($files as $file)
                               <tr class="redirect-click" data-redirect="{{ route('file', $file->id) }}">
-                                <td>{{$file->brand}} {{$file->vehicle()->Name}} {{ $file->engine }} {{ $file->vehicle()->TORQUE_standard }}</td>
-                                <td><span class="label label-success">{{$file->status}}</span></td>
-                                <td><span class="badge badge-important">{{$file->credits}}</span></td>
+                                <td><span class="label label-success">Task{{$file->id}}</span></td>
                                 <td>{{$file->name}}</td>
-                                <td>{{$file->created_at->diffForHumans();}}</td>
+                                <td>{{$file->brand}} {{ $file->engine }} {{ $file->vehicle()->TORQUE_standard }}</td>
+                                <td><span class="label label-success">{{$file->file_type}}</span></td>
+                                <td><span class="label label-success">{{$file->status}}</span></td>
+                                <td>
+                                  <div class="">
+                                    @if($file->stages)
+                                      <img alt="{{$file->stages}}" width="33" height="33" data-src-retina="{{ url('icons').'/'.\App\Models\Service::where('name', $file->stages)->first()->icon }}" data-src="{{ url('icons').'/'.\App\Models\Service::where('name', $file->stages)->first()->icon }}" src="{{ url('icons').'/'.\App\Models\Service::where('name', $file->stages)->first()->icon }}">
+                                      <span class="text-black" style="top: 2px; position:relative;">{{ $file->stages }}</span>
+                                    @endif
+                                  </div>  
+                                </td>
+                                <td>
+                                  <div class="">
+                                    @if($file->options)
+                      
+                                      @foreach($file->options() as $option)
+                                        <span class="label label-warning-darker m-l-10" class="text-black" style="top: 2px; position:relative;">
+                                          <img alt="{{$option}}" width="20" height="20" data-src-retina="{{ url('icons').'/'.\App\Models\Service::where('name', $option)->first()->icon }}" data-src="{{ url('icons').'/'.\App\Models\Service::where('name', $option)->first()->icon }}" src="{{ url('icons').'/'.\App\Models\Service::where('name', $option)->first()->icon }}">
+                                          {{ $option }}
+                                        </span>
+                                      @endforeach
+                       
+                                    @endif
+                                  </div>  
+                                </td>
+                                <td><span class="badge badge-important">{{$file->credits}}</span></td>
+                                
+                                <td>{{\Carbon\Carbon::parse($file->created_at)->format('d/m/Y H:i: A')}}</td>
                               </tr>
                           @endforeach
                         </tbody>
