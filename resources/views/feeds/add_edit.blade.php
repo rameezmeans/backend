b851cdd89f948a04ac0d5af3201ac62d287c1798
Rameez Bhatti <xrkalix@gmail.com>
2023-01-03 22:11:40 +0500

    ui for feeds

--- resources/views/feeds/add_edit.blade.php
+++ resources/views/feeds/add_edit.blade.php
@@ -10,7 +10,7 @@
           <div class="card card-transparent m-t-40">
             <div class="card-header ">
                 <div class="card-title">
-                  @if(isset($engineer))
+                  @if(isset($feed))
                   <h5>
                     Edit News Feed
                   </h5>
@@ -58,45 +58,100 @@
                   </span>
                 @enderror
 
-                <div class="form-group form-group-default ">
-                  <label>Activation Day of Week</label>
-                  <select class="full-width" data-init-plugin="select2" name="activation_weekday">
-                    <option value="" disabled selected>Select A Day</option>
-                    <option @if(isset($feed) && $feed->activation_weekday == 'Sunday') selected @endif value="Sunday">Sunday</option>
-                    <option  @if(isset($feed) && $feed->activation_weekday == 'Monday') selected @endif value="monday">Monday</option>
-                    <option  @if(isset($feed) && $feed->activation_weekday == 'Tuesday') selected @endif value="Tuesday">Tuesday</option>
-                    <option  @if(isset($feed) && $feed->activation_weekday == 'Wednesday') selected @endif value="Wednesday">Wednesday</option>
-                    <option  @if(isset($feed) && $feed->activation_weekday == 'Thursday') selected @endif value="Thursday">Thursday</option>
-                    <option  @if(isset($feed) && $feed->activation_weekday == 'Friday') selected @endif value="Friday">Friday</option>
-                    <option  @if(isset($feed) && $feed->activation_weekday == 'Saturday') selected @endif value="Saturday">Saturday</option>
+                <div class="form-group form-group-default required ">
+                  <label>Type</label>
+                  <select class="full-width" data-init-plugin="select2" name="type">
+                    <option @if(isset($feed) && $feed->type == 'danger') selected @endif value="danger">Danger</option>
+                    <option  @if(isset($feed) && $feed->type == 'warning') selected @endif value="warning">Warning</option>
+                    <option  @if(isset($feed) && $feed->language == 'good_news') selected @endif value="good_news">Good New</option>
                   </select>
                 </div>
-              
-              @error('activation_weekday')
-                <span class="text-danger" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-              @enderror
+                @error('type')
+                  <span class="text-danger" role="alert">
+                      <strong>{{ $message }}</strong>
+                  </span>
+                @enderror
+                <h5 class="m-t-40 m-b-30">
+                  Activation Method
+                </h5>
+                <div class="card card-transparent ">
+                  <!-- Nav tabs -->
+                  <ul class="nav nav-tabs nav-tabs-fillup d-none d-md-flex d-lg-flex d-xl-flex" data-init-reponsive-tabs="dropdownfx">
+                    <li class="nav-item">
+                      <a href="#" class="active show" data-toggle="tab" data-target="#slide1"><span>Days of Week</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="#" data-toggle="tab" data-target="#slide2" class=""><span>Date and Time</span></a>
+                    </li>
+                  </ul>
+                  <div class="nav-tab-dropdown cs-wrapper full-width d-lg-none d-xl-none d-md-none"><div class="cs-select cs-skin-slide full-width" tabindex="0"><span class="cs-placeholder">Hello World</span><div class="cs-options"><ul><li data-option="" data-value="#slide1"><span>Home</span></li><li data-option="" data-value="#slide2"><span>Profile</span></li><li data-option="" data-value="#slide3"><span>Messages</span></li></ul></div><select class="cs-select cs-skin-slide full-width" data-init-plugin="cs-select"><option value="#slide1" selected="">Home</option><option value="#slide2">Profile</option><option value="#slide3">Messages</option></select><div class="cs-backdrop"></div></div></div>
+                 
+                  <!-- Tab panes -->
+                  <div class="tab-content">
+                    <div class="tab-pane slide-left active show" id="slide1">
+                      <div class="row column-seperation">
+                        <div class="col-lg-12">
+                          <div class="form-group form-group-default ">
+                            <label>Activation Day of Week</label>
+                            <select class="full-width" data-init-plugin="select2" name="activation_weekday">
+                              <option value="" disabled selected>Select A Day</option>
+                              <option @if(isset($feed) && $feed->activation_weekday == 'Sunday') selected @endif value="Sunday">Sunday</option>
+                              <option  @if(isset($feed) && $feed->activation_weekday == 'Monday') selected @endif value="monday">Monday</option>
+                              <option  @if(isset($feed) && $feed->activation_weekday == 'Tuesday') selected @endif value="Tuesday">Tuesday</option>
+                              <option  @if(isset($feed) && $feed->activation_weekday == 'Wednesday') selected @endif value="Wednesday">Wednesday</option>
+                              <option  @if(isset($feed) && $feed->activation_weekday == 'Thursday') selected @endif value="Thursday">Thursday</option>
+                              <option  @if(isset($feed) && $feed->activation_weekday == 'Friday') selected @endif value="Friday">Friday</option>
+                              <option  @if(isset($feed) && $feed->activation_weekday == 'Saturday') selected @endif value="Saturday">Saturday</option>
+                            </select>
+                          </div>
+                        
+                          @error('activation_weekday')
+                            <span class="text-danger" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                          @enderror
+          
+                          <div class="form-group form-group-default ">
+                            <label>Deactivation Day of Week</label>
+                            <select class="full-width" data-init-plugin="select2" name="deactivation_weekday">
+                              <option value="" disabled selected>Select A Day</option>
+                              <option @if(isset($feed) && $feed->deactivation_weekday == 'Sunday') selected @endif value="Sunday">Sunday</option>
+                              <option  @if(isset($feed) && $feed->deactivation_weekday == 'Monday') selected @endif value="Monday">Monday</option>
+                              <option  @if(isset($feed) && $feed->deactivation_weekday == 'Tuesday') selected @endif value="Tuesday">Tuesday</option>
+                              <option  @if(isset($feed) && $feed->deactivation_weekday == 'Wednesday') selected @endif value="Wednesday">Wednesday</option>
+                              <option  @if(isset($feed) && $feed->deactivation_weekday == 'Thursday') selected @endif value="Thursday">Thursday</option>
+                              <option  @if(isset($feed) && $feed->deactivation_weekday == 'Friday') selected @endif value="Friday">Friday</option>
+                              <option  @if(isset($feed) && $feed->deactivation_weekday == 'Saturday') selected @endif value="Saturday">Saturday</option>
+                            </select>
+                          </div>
+                      
+                          @error('deactivation_weekday')
+                            <span class="text-danger" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                          @enderror
+                        </div>
+                      </div>
+                    </div>
+                    <div class="tab-pane slide-left" id="slide2">
+                      <div class="row">
+                        <div class="col-lg-12">
+                          <label>Activation Range</label>
+                            <div class="input-group m-b-10">
+                            
+                              <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fa fa-calendar"></i></span>
+                              </div>
+                              <input type="text" name="dateTimeRange" id="daterangepicker" class="form-control" value="@if(isset($feed)){{ \Carbon\Carbon::parse($feed->activate_at)->format('d/m/Y h:i A') }}@else{{date('d/m/Y h:i A')}}@endif - @if(isset($feed)){{\Carbon\Carbon::parse($feed->deactivate_at)->format('d/m/Y h:i A')}}@else{{date('d/m/Y h:i A')}}@endif">
+                            </div>
+                          
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
 
-              <div class="form-group form-group-default ">
-                <label>Deactivation Day of Week</label>
-                <select class="full-width" data-init-plugin="select2" name="deactivation_weekday">
-                  <option value="" disabled selected>Select A Day</option>
-                  <option @if(isset($feed) && $feed->deactivation_weekday == 'Sunday') selected @endif value="Sunday">Sunday</option>
-                  <option  @if(isset($feed) && $feed->deactivation_weekday == 'Monday') selected @endif value="Monday">Monday</option>
-                  <option  @if(isset($feed) && $feed->deactivation_weekday == 'Tuesday') selected @endif value="Tuesday">Tuesday</option>
-                  <option  @if(isset($feed) && $feed->deactivation_weekday == 'Wednesday') selected @endif value="Wednesday">Wednesday</option>
-                  <option  @if(isset($feed) && $feed->deactivation_weekday == 'Thursday') selected @endif value="Thursday">Thursday</option>
-                  <option  @if(isset($feed) && $feed->deactivation_weekday == 'Friday') selected @endif value="Friday">Friday</option>
-                  <option  @if(isset($feed) && $feed->deactivation_weekday == 'Saturday') selected @endif value="Saturday">Saturday</option>
-                </select>
-              </div>
-            
-            @error('deactivation_weekday')
-              <span class="text-danger" role="alert">
-                  <strong>{{ $message }}</strong>
-              </span>
-            @enderror
+                
                 
 
                 {{-- <div class="form-group form-group-default required ">
@@ -117,28 +172,8 @@
                       <strong>{{ $message }}</strong>
                   </span>
                 @enderror --}}
-                <div class="form-group form-group-default required ">
-                  <label>Type</label>
-                  <select class="full-width" data-init-plugin="select2" name="type">
-                    <option @if(isset($feed) && $feed->type == 'danger') selected @endif value="danger">Danger</option>
-                    <option  @if(isset($feed) && $feed->type == 'warning') selected @endif value="warning">Warning</option>
-                    <option  @if(isset($feed) && $feed->language == 'good_news') selected @endif value="good_news">Good New</option>
-                  </select>
-                </div>
+                
               
-              @error('type')
-                <span class="text-danger" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-              @enderror
-              <label>Activation Range</label>
-                <div class="input-group m-b-10">
-                 
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="fa fa-calendar"></i></span>
-                  </div>
-                  <input type="text" name="dateTimeRange" id="daterangepicker" class="form-control" value="@if(isset($feed)){{ \Carbon\Carbon::parse($feed->activate_at)->format('d/m/Y h:i A') }}@else{{date('d/m/Y h:i A')}}@endif - @if(isset($feed)){{\Carbon\Carbon::parse($feed->deactivate_at)->format('d/m/Y h:i A')}}@else{{date('d/m/Y h:i A')}}@endif">
-                </div>
                 <div class="text-center m-t-40">                    
                   <button class="btn btn-success btn-cons m-b-10" type="submit"><i class="pg-plus_circle"></i> <span class="bold">@if(isset($feed)) Update @else Add @endif</span></button>
                   @if(isset($feed))
