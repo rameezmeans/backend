0abde293fc3f72ef6e59dd1e980de9d45ceb7299
Rameez Bhatti <xrkalix@gmail.com>
2023-01-11 18:48:40 +0500

    access level

--- app/Http/Controllers/EmailTemplatesController.php
+++ app/Http/Controllers/EmailTemplatesController.php
@@ -15,6 +15,7 @@ class EmailTemplatesController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('adminOnly');
     }
 
     public function index() {
