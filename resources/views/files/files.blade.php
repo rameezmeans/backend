37b7d5274c35d88c2e00cdfa09fb65a2eb4e7414
Rameez Bhatti <xrkalix@gmail.com>
2023-02-16 06:33:19 +0500

    backgrounds for file

--- resources/views/files/files.blade.php
+++ resources/views/files/files.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
+@section('pagespecificstyles')
+  <style>
+    .checked-darkly {
+      background-color: #d8dadc !important;
+    }
+  </style>
+@endsection
+
 @section('content')
 <div class="page-content-wrapper ">
     <!-- START PAGE CONTENT -->
@@ -36,7 +44,7 @@
                         </thead>
                         <tbody>
                           @foreach($files as $file)
-                              <tr class="redirect-click @if($file->checked_by == 'customer') checked @endif" data-redirect="{{ route('file', $file->id) }}">
+                              <tr class="redirect-click @if($file->checked_by == 'customer') checked-darkly @endif" data-redirect="{{ route('file', $file->id) }}">
                                 <td><span class="label @if($file->frontend->id == 1) text-white bg-primary @else text-black bg-warning @endif">Task{{$file->id}}</span></td>
                                 <td>{{$file->user->name}}</td>
                                 <td>{{$file->brand}} {{ $file->engine }} {{ $file->vehicle()->TORQUE_standard }}</td>
