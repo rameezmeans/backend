de61ce89d167d6b264a9435aeccafc305b564678
Rameez Bhatti <xrkalix@gmail.com>
2023-04-02 12:22:27 +0500

    deleting file

--- app/Http/Controllers/FilesAPIController.php
+++ app/Http/Controllers/FilesAPIController.php
@@ -46,7 +46,7 @@ class FilesAPIController extends Controller
             copy( public_path('/../../portal/public/uploads/filesready').'/'.$file->tunned_files->file, 
             public_path('/../../portal/public'.$file->file_path.$file->tunned_files->file) );
 
-            unlink( public_path('/../../portal/uploads/filesready').'/'.$request->tuned_file );
+            unlink( public_path('/../../portal/public/uploads/filesready').'/'.$file->tunned_files->file );
 
             $engineerFile = new RequestFile();
             $engineerFile->request_file = $request->tuned_file;
