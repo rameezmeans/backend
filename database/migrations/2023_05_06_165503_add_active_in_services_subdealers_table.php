e65cc07936b35e66fbd8c203fa58f5d247dc7865
Rameez Bhatti <xrkalix@gmail.com>
2023-05-06 20:39:06 +0500

    activate subdealers

---
+++ database/migrations/2023_05_06_165503_add_active_in_services_subdealers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('services_subdealer_groups', function (Blueprint $table) {
+            $table->boolean('active')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+       
+    }
+};
