2d0cfdbea89e9ee30bf60c6eb48c8a5a2aed9d9e
Rameez Bhatti <xrkalix@gmail.com>
2023-08-29 19:13:00 +0500

    test tokens

--- app/Http/Controllers/SubdealerGroupsController.php
+++ app/Http/Controllers/SubdealerGroupsController.php
@@ -118,7 +118,8 @@ class SubdealerGroupsController extends Controller
         $evcPassword = Key::where('subdealer_group_id', $id)
         ->where('key', 'evc_password')->first();
 
-
+        dd($evcUsername);
+        
         return view('subdealers.edit_tokens', 
         [
             'subdealerID' => $id,
