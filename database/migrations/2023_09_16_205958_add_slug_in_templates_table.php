9a3dccfa2ffb84fef117332b6faa75c277face73
Rameez Bhatti <xrkalix@gmail.com>
2023-09-16 23:08:54 +0500

    slug in templates

---
+++ database/migrations/2023_09_16_205958_add_slug_in_templates_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('templates', function (Blueprint $table) {
+
+            Schema::table('email_templates', function (Blueprint $table) {
+                $table->string('slug')->nullable();
+            });
+    
+            Schema::table('message_templates', function (Blueprint $table) {
+                $table->string('slug')->nullable();
+            });
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('templates', function (Blueprint $table) {
+            //
+        });
+    }
+};
