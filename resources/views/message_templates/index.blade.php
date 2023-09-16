46f55ebcacc364c0c11bee94c2dc3551cc17e628
Rameez Bhatti <xrkalix@gmail.com>
2023-09-16 20:20:57 +0500

    front end id in templates

--- resources/views/message_templates/index.blade.php
+++ resources/views/message_templates/index.blade.php
@@ -14,8 +14,8 @@
                 </div>
                 <div class="pull-right">
                 <div class="col-xs-12">
-                    <button data-redirect="{{ route('add-message-template') }}" class="btn btn-success btn-cons m-b-10 redirect-click" type="button"><i class="pg-plus_circle"></i> <span class="bold">Create Message Template</span>
-                    </button>
+                    {{-- <button data-redirect="{{ route('add-message-template') }}" class="btn btn-success btn-cons m-b-10 redirect-click" type="button"><i class="pg-plus_circle"></i> <span class="bold">Create Message Template</span>
+                    </button> --}}
                     {{-- <input type="text" id="search-table" class="form-control pull-right" placeholder="Search"> --}}
                 </div>
                 </div>
