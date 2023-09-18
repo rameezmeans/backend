a067c5639ce4df643a715fb4ccbe6ddf944e7b70
Rameez Bhatti <xrkalix@gmail.com>
2023-09-18 10:09:51 +0500

    lua settings page done

--- resources/views/files/show.blade.php
+++ resources/views/files/show.blade.php
@@ -1635,11 +1635,7 @@
 
         console.log(numberofDecodedFiles);
 
-        /*
-          if(numberofDecodedFiles > 0) 
-          then 
-          var file_loc ="<? echo $file->final_decoded_file;?>";
-        */
+        
 
         var file_loc ="<? echo $file->file_attached;?>";
         
