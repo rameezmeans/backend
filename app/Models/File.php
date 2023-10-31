573254c67b99a93ba587c9160cf3ad97bf00ed5d
Rameez Bhatti <xrkalix@gmail.com>
2023-10-31 21:38:22 +0500

    subject and sender thing

--- app/Models/File.php
+++ app/Models/File.php
@@ -123,9 +123,9 @@ class File extends Model
     public function getECUComment(){
 
         $vehicle = Vehicle::where('Make', '=', $this->brand)
-        ->where('Model', '=', $this->model)
-        ->where('Generation', '=', $this->version)
-        ->where('Engine', '=', $this->engine)
+        // ->where('Model', '=', $this->model)
+        // ->where('Generation', '=', $this->version)
+        // ->where('Engine', '=', $this->engine)
         ->first();
 
         return $vehicle->getComment($this->ecu);
