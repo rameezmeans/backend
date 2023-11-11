b4f52d7947802f2a80c129099e19c7422dedbe28
Rameez Bhatti <xrkalix@gmail.com>
2023-11-11 17:28:00 +0500

    elorus thing

--- routes/web.php
+++ routes/web.php
@@ -117,7 +117,7 @@ Route::get('/info', function () {
     foreach($credits as $credit){
         $user = User::findOrFail($credit->user_id);
         $credit->front_end_id = $user->front_end_id;
-        $credit->elorus_able = !$user->test;
+        $credit->elorus_able = ($user->test) ? 0: 1;
     }
 
     dd('done');
