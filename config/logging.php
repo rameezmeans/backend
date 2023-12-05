2b44482c205b3d663692564b24908f42578cd43e
Rameez Bhatti <xrkalix@gmail.com>
2023-12-05 10:02:13 +0500

    flare added

--- config/logging.php
+++ config/logging.php
@@ -53,8 +53,12 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily', 'flare'],
             'ignore_exceptions' => false,
+        ],
+
+        'flare' => [
+            'driver' => 'flare',
         ],
 
         'single' => [
