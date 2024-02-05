3c5dc3e79cd3691e38b165e094423f7230a8fe1a
Rameez Bhatti <xrkalix@gmail.com>
2024-02-05 16:21:08 +0500

    file status thing fixed

--- app/Http/Controllers/AlientechController.php
+++ app/Http/Controllers/AlientechController.php
@@ -491,9 +491,9 @@ class AlientechController extends Controller
                 $url = "https://encodingapi.alientech.to/api/kess3/encode-boot-bench-file";
             }
 
-            // else {
-            //     $url = "https://encodingapi.alientech.to/api/kess3/encode-obd-file";
-            // }
+            else {
+                $url = "https://encodingapi.alientech.to/api/kess3/encode-obd-file";
+            }
 
             $headers = [
             'X-Alientech-ReCodAPI-LLC' => $this->token,
@@ -511,6 +511,14 @@ class AlientechController extends Controller
                     'userCustomerCode' => 'user1',
                     'kess3FileSlotGUID' => $slotID,
                     'microFileGUID' => $response->guid,
+                ];
+            }
+            else{
+                
+                $postInput = [
+                    'userCustomerCode' => 'user1',
+                    'kess3FileSlotGUID' => $slotID,
+                    'modifiedFileGUID' => $response->guid,
                 ];
             }
 
