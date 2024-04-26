153c74937cc8eb13fc0dee94dcd0271af6170081
Rameez Bhatti <xrkalix@gmail.com>
2024-04-26 17:36:16 +0500

    devback storage

---
+++ database/migrations/2024_04_26_152334_add_active_payment_account_in_groups_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('groups', function (Blueprint $table) {
+
+            $table->boolean('stripe_active')->default('true');
+            $table->boolean('paypal_active')->default('true');
+            $table->boolean('viva_active')->default('false');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('groups', function (Blueprint $table) {
+            //
+        });
+    }
+};
