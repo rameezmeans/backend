d929f3d4ac635a34a251de4ab20abdef20a5771f
Rameez Bhatti <xrkalix@gmail.com>
2024-05-25 21:18:16 +0500

    comments thing

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -2877,7 +2877,7 @@ class FilesController extends Controller
         else{
             $comments = null;
         }
-        
+        dd($comments);
         $showComments = false;
 
         $selectedOptions = [];
