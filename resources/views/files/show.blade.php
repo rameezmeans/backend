488864f8634d28e72411e1f5c238e0676725fc69
Rameez Bhatti <xrkalix@gmail.com>
2024-06-03 18:00:53 +0500

    thing

--- resources/views/files/show.blade.php
+++ resources/views/files/show.blade.php
@@ -2861,7 +2861,7 @@ margin-bottom: 10px !important;
                                 <a href="#" data-toggle="tab" data-target="#acm_data_{{$message['id']}}"><span>ACM Information</span></a>
                               </li>
                               <li class="nav-item">
-                                <a href="#" data-toggle="tab" data-target="#software_data{{$message['id']}}"><span>Messages</span></a>
+                                <a href="#" data-toggle="tab" data-target="#software_data_{{$message['id']}}"><span>Messages</span></a>
                               </li>
                             </ul>
                             <!-- Tab panes -->
