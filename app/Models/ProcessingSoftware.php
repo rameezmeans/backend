fadb80b88d7f5a7a6d8c5a0706769b51fd45ea1e
Rameez Bhatti <xrkalix@gmail.com>
2024-06-04 20:25:45 +0500

    software thing

--- app/Models/ProcessingSoftware.php
+++ app/Models/ProcessingSoftware.php
@@ -13,6 +13,6 @@ class ProcessingSoftware extends Model
     protected $table = "processing_softwares";
 
     public function files(){
-        return FileReplySoftwareService::where('software_id', $this->id)->distinct()->get();
+        return FileReplySoftwareService::where('software_id', $this->id)->distinct('file_id')->get();
     }
 }
