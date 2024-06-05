231b458ca7d306f5e9a0fb718950e69c430e8659
Rameez Bhatti <xrkalix@gmail.com>
2024-06-05 22:28:32 +0500

    test thing

---
+++ database/migrations/2024_06_05_202647_add_columns_in_tables_for_test.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('files', function (Blueprint $table) {
+            $table->boolean('test')->default(0);
+        });
+
+        Schema::table('credits', function (Blueprint $table) {
+            $table->boolean('test')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tables_for_test', function (Blueprint $table) {
+            //
+        });
+    }
+};
