9251eac0ec89e39ae1a0275419a39eb9788cb329
Rameez Bhatti <xrkalix@gmail.com>
2024-07-11 15:54:20 +0500

    unit price thing

---
+++ database/migrations/2024_07_11_135149_add_unit_price_in_prices_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('prices', function (Blueprint $table) {
+            $table->float('unit_price')->default(10.0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('prices', function (Blueprint $table) {
+            //
+        });
+    }
+};
