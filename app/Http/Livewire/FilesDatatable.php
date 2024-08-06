c07a9f0236b6975949444d6aecab25cf514f4d4a
Rameez Bhatti <xrkalix@gmail.com>
2024-08-06 20:09:02 +0500

    file thing

--- app/Http/Livewire/FilesDatatable.php
+++ app/Http/Livewire/FilesDatatable.php
@@ -145,7 +145,7 @@ class FilesDatatable extends LivewireDatatable
                 
                 foreach($file->options_services as $option){
                     if(\App\Models\Service::findOrFail($option->service_id) != null){
-                        $service = \App\Models\Service::where('id',$option->service_id)->first;
+                        $service = \App\Models\Service::where('id',$option->service_id)->first();
 
                             if($service){
                                 $options .= '<img class="parent-adjusted" alt="'.$service->name.'" width="30" height="30" data-src-retina="'.url('icons').'/'.$service->icon .'" data-src="'.url('icons').'/'.$service->icon .'" src="'.url('icons').'/'.$service->icon.'">';
