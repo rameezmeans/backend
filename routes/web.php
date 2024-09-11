3c290697daa2ff83b5f44d4ec214a4f564a7adb4
Rameez Bhatti <xrkalix@gmail.com>
2024-09-11 16:14:53 +0500

    excel thing

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,8 @@ use Twilio\Rest\Client;
 use SevenSpan\WhatsApp\WhatsApp;
 
 use Chatify\Facades\ChatifyMessenger as Chatify;
+use Maatwebsite\Excel\Facades\Excel;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -90,6 +92,9 @@ Route::get('/all_files_with_software/{id}/{service_id}/{software_id}', function 
 
 Route::get('/tasks', function () {
 
+    $excel = Excel::load(public_path('codes.xlsx'), function($reader) {})->get();
+    dd($excel);
+    
     // $creditsWithoutZohoID = Credit::whereNull('zohobooks_id')
     //     ->where('credits','>', 0)
     //     ->where('gifted', 0)
