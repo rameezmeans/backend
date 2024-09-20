3b0c7e20ad2425880160a13998dfe0803fc71cac
Rameez Bhatti <xrkalix@gmail.com>
2024-09-21 03:36:23 +0500

    show comments thing

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -114,7 +114,7 @@ class UsersController extends Controller
                     ->whereRaw('date(created_at) = curdate()')
                     ->where('test', 0)
                     ->where('front_end_id', $request->front_end)->get('id')->toArray();
-                    dd('here');
+                    
                     dd($users);
 
                 }
