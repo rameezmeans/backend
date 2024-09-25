7e10cb138c022746674b385812b705d23a3fe159
Rameez Bhatti <xrkalix@gmail.com>
2024-09-26 03:13:20 +0500

    mega report

--- resources/views/services/mega.blade.php
+++ resources/views/services/mega.blade.php
@@ -36,7 +36,7 @@
                             <p>Start Date: <span>{{$start}}</span></p>
                             <p>End Date: <span>{{$end}}</span></p>
                             <p>Frontend: <span>{{\App\Models\FrontEnd::findOrFail($frontend)->name}}</span></p>
-                            <p>Frontend: <span>{{$countries}}</span></p>
+                            <p>Frontend: <span>{{implode(',',$countries)}}</span></p>
                         </div>
                     
 
