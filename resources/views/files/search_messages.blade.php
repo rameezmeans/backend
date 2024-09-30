1427319c9ed8b5ed386fff486323a1325a7fd560
Rameez Bhatti <xrkalix@gmail.com>
2024-09-30 13:13:21 +0500

    edit options comment

--- resources/views/files/search_messages.blade.php
+++ resources/views/files/search_messages.blade.php
@@ -27,7 +27,7 @@
                             <div class="form-group form-group-default input-group">
                                 <div class="form-input-group">
                                     <label>Keyword</label>
-                                    <input type="input" style="margin-bottom: 17px;" class="form-control" placeholder="Put Your Text here" name="keyword" value="@if($_POST('keyword') != NULL) {{$_POST('keyword')}} @endif">
+                                    <input type="input" style="margin-bottom: 17px;" class="form-control" placeholder="Put Your Text here" name="keyword" value="@if($request->keyword != NULL){{$request->keyword}}@endif">
                                 </div>
                                 
 
