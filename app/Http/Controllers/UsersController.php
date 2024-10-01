a73c53e2b6754284f8acc5a64f2b1a3a0b468cba
Rameez Bhatti <xrkalix@gmail.com>
2024-10-01 05:29:25 +0500

    edit options comment

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -189,11 +189,11 @@ class UsersController extends Controller
                 // ->get();
 
                 $countries = File::join('users', 'files.user_id', '=', 'users.id')
-                ->select('users.country', \DB::raw("count(users.id) as count"))
+                ->select('*', \DB::raw("count(users.id) as count"))
                 ->groupby('users.country')
                 ->orderBy('count', 'desc')
-                // ->whereDate('files.created_at', '>=' , $startDate)
-                // ->whereDate('files.created_at', '<=' , $endDate)
+                ->whereDate('files.created_at', '>=' , $startDate)
+                ->whereDate('files.created_at', '<=' , $endDate)
                 ->where('files.front_end_id', $request->front_end)
                 ->get();
 
