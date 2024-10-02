76f71723e9f7e7ee5edc3b82646ead9d383bbbd0
Rameez Bhatti <xrkalix@gmail.com>
2024-10-02 19:37:03 +0500

    table thing

--- resources/views/groups/create_edit_customers.blade.php
+++ resources/views/groups/create_edit_customers.blade.php
@@ -428,7 +428,7 @@
 
                 <div class="form-group form-group-default required ">
                   <label>Elorus Account ID</label>
-                  <input value="@if(isset($customer)){{$customer->elorus_id}}@endif"  name="elorus_id" type="text" class="form-control" autocomplete="off">
+                  <input value="@if(isset($customer) && $customer->elorus_id != NULL){{$customer->elorus_id}}@endif"  name="elorus_id" type="text" class="form-control" autocomplete="off">
                 </div>
                 @error('elorus_id')
                   <span class="text-danger" role="alert">
