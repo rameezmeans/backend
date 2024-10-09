0cd38d73db2ccbae134bea2e7e8e9832ee97269b
Rameez Bhatti <xrkalix@gmail.com>
2024-10-09 19:50:12 +0500

    modal thing

--- resources/views/groups/create_edit_customers.blade.php
+++ resources/views/groups/create_edit_customers.blade.php
@@ -515,7 +515,7 @@
     </div>
 </div>
 
-<div class="modal fade slide-up disable-scroll" id="testModal" tabindex="-1" role="dialog" aria-hidden="false">
+<div class="modal fade stick-up disable-scroll" id="testModal" tabindex="-1" role="dialog" aria-hidden="false">
   <div class="modal-dialog ">
     <div class="modal-content-wrapper">
       <div class="modal-content">
