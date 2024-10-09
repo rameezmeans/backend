9562fe0109d7e43362f1bb4869cb3b08a0ef6a22
Rameez Bhatti <xrkalix@gmail.com>
2024-10-09 20:37:10 +0500

    modal thing

--- resources/views/groups/create_edit_customers.blade.php
+++ resources/views/groups/create_edit_customers.blade.php
@@ -514,6 +514,7 @@
               </h5>
 
               <form action="{{route('update-test-status')}}" method="POST" role="form">
+                @csrf
                 <div class="">
                   <div class="row">
                     <div class="col-md-12">
