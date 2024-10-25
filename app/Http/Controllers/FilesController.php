e86b04eeb8475f68e5011109473d382b9c740e12
Rameez Bhatti <xrkalix@gmail.com>
2024-10-25 19:13:12 +0500

    latest thing

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -2410,7 +2410,16 @@ class FilesController extends Controller
 
         $reply->engineer = true;
         $reply->file_id = $request->file_id;
-        $reply->request_file_id = $request->request_file_id;
+
+        $latest = EngineerFileNote::where('file_id', $request->file_id)->latest();
+
+        if($latest->request_file_id != NULL){
+            $reply->request_file_id = $latest->request_file_id;
+        }
+        else{
+            $reply->request_file_id = NULL;
+        }
+
         $reply->save();
         
         $file->support_status = "closed";
