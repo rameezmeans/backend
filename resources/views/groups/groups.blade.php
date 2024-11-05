92fa23d2eeb949c3b33bc2bc501e0fa5d23ba646
Rameez Bhatti <xrkalix@gmail.com>
2024-11-05 09:26:55 +0500

    frontend thing

--- resources/views/groups/groups.blade.php
+++ resources/views/groups/groups.blade.php
@@ -87,8 +87,66 @@
 
                         </div>
 
-                        <div class="tab-pane slide-left" id="slide2"></div>
-                        <div class="tab-pane slide-left" id="slide3"></div>
+                        <div class="tab-pane slide-left" id="slide2">
+                            <div id="tableWithSearch_wrapper" class="dataTables_wrapper no-footer m-t-40">
+                                <div>
+                                    <table class="table table-hover demo-table-search table-responsive-block dataTable no-footer" id="tableWithSearch" role="grid" aria-describedby="tableWithSearch_info">
+                                        <thead>
+                                            <tr role="row">
+                                                <th class="" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Title: activate to sort column descending">Name</th>
+                                                <th class="" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Title: activate to sort column descending">Frontend</th>
+                                                <th class="" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Title: activate to sort column descending">Created At</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($tuningXGroups as $group)
+                                                <tr role="row" @if(Auth::user()->is_admin() || get_engineers_permission(Auth::user()->id, 'edit-groups')) class="redirect-click" data-redirect="{{ route('edit-group', $group->id) }}" @endif>
+                                                    <td class="v-align-middle semi-bold sorting_1">
+                                                        <p>{{$group->name}}</p>
+                                                    </td>
+                                                    <td class="v-align-middle semi-bold sorting_1">
+                                                        <p><label class="label @if($group->front_end_id == 1) text-white bg-primary @elseif($group->front_end_id == 3) text-white bg-info @else text-black bg-warning @endif">{{$group->frontend->name}}</label></p>
+                                                      </td>
+                                                    <td class="v-align-middle semi-bold sorting_1">
+                                                        <p>{{$group->created_at->diffForHumans()}}</p>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane slide-left" id="slide3">
+                            <div id="tableWithSearch_wrapper" class="dataTables_wrapper no-footer m-t-40">
+                                <div>
+                                    <table class="table table-hover demo-table-search table-responsive-block dataTable no-footer" id="tableWithSearch" role="grid" aria-describedby="tableWithSearch_info">
+                                        <thead>
+                                            <tr role="row">
+                                                <th class="" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Title: activate to sort column descending">Name</th>
+                                                <th class="" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Title: activate to sort column descending">Frontend</th>
+                                                <th class="" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Title: activate to sort column descending">Created At</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($etfGroups as $group)
+                                                <tr role="row" @if(Auth::user()->is_admin() || get_engineers_permission(Auth::user()->id, 'edit-groups')) class="redirect-click" data-redirect="{{ route('edit-group', $group->id) }}" @endif>
+                                                    <td class="v-align-middle semi-bold sorting_1">
+                                                        <p>{{$group->name}}</p>
+                                                    </td>
+                                                    <td class="v-align-middle semi-bold sorting_1">
+                                                        <p><label class="label @if($group->front_end_id == 1) text-white bg-primary @elseif($group->front_end_id == 3) text-white bg-info @else text-black bg-warning @endif">{{$group->frontend->name}}</label></p>
+                                                      </td>
+                                                    <td class="v-align-middle semi-bold sorting_1">
+                                                        <p>{{$group->created_at->diffForHumans()}}</p>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
 
