37bf698f6e5eaf7139a9bf64489e10fd20361183
Rameez Bhatti <xrkalix@gmail.com>
2024-11-26 16:21:53 +0500

    4 tables

---
+++ database/migrations/2024_11_26_114025_create_4_tables.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paypal_records', function (Blueprint $table) {
+            $table->id();
+            $table->string('paypal_id');
+            $table->float('amount');
+            $table->float('tax');
+            $table->string('desc');
+            $table->timestamps();
+        });
+
+        Schema::create('stripe_records', function (Blueprint $table) {
+            $table->id();
+            $table->string('stripe_id');
+            $table->float('amount');
+            $table->float('tax');
+            $table->string('desc');
+            $table->timestamps();
+        });
+
+        Schema::create('elorus_records', function (Blueprint $table) {
+            $table->id();
+            $table->string('elorus_id');
+            $table->float('amount');
+            $table->float('tax');
+            $table->string('desc');
+            $table->timestamps();
+        });
+
+        Schema::create('zoho_records', function (Blueprint $table) {
+            $table->id();
+            $table->string('zoho_id');
+            $table->float('amount');
+            $table->float('tax');
+            $table->string('desc');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('4_tables');
+    }
+};
