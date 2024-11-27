4f14b6f99ff7ddf85b6b715138e522661a2bcc9b
Rameez Bhatti <xrkalix@gmail.com>
2024-11-27 13:07:24 +0500

    details page

---
+++ resources/views/payment_logs/details.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="page-content-wrapper ">
+    <!-- START PAGE CONTENT -->
+    <div class="content sm-gutter">
+      <!-- START CONTAINER FLUID -->
+        <div class=" container-fluid   container-fixed-lg bg-white">
+
+          <div class="card card-transparent m-t-40">
+            <div class="card-header ">
+                <div class="card-title"><h3>Details</h3>
+                </div>
+                <div class="pull-right">
+                <div class="col-xs-12">
+                    
+                </div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <div class="card-body">
+                
+            </div>
+          </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+
+@section('pagespecificscripts')
+
+<script type="text/javascript">
+
+    $( document ).ready(function(event) {
+        
+      
+    });
+
+</script>
+
+@endsection
