069900723d6b9af596ca80581b013454b8260392
Rameez Bhatti <xrkalix@gmail.com>
2024-12-04 12:14:09 +0500

    button there

--- database/migrations/2024_11_30_082324_create_file_event_table.php
+++ database/migrations/2024_11_30_082324_create_file_event_table.php
@@ -15,21 +15,21 @@ return new class extends Migration
     {
         Schema::create('file_event', function (Blueprint $table) {
 
-            $table->id();
+            // $table->id();
 
-            $table->foreignId('temporary_file_id');
-            $table->time('file_uploaded', precision: 0);
-            $table->integer('credits_at_start');
+            // $table->foreignId('temporary_file_id');
+            // $table->time('file_uploaded', precision: 0);
+            // $table->integer('credits_at_start');
 
-            $table->foreignId('stages')->default(0);
-            $table->string('options')->nullable();
-            $table->time('time_to_pick_stages_and_options', precision: 0)->nullable();
-            $table->integer('credits_at_stages_and_options')->default(0);
-            $table->integer('credits_for_stagees_and_options')->default(0);
+            // $table->foreignId('stages')->default(0);
+            // $table->string('options')->nullable();
+            // $table->time('time_to_pick_stages_and_options', precision: 0)->nullable();
+            // $table->integer('credits_at_stages_and_options')->default(0);
+            // $table->integer('credits_for_stagees_and_options')->default(0);
 
-            $tab
+            // $tab
 
-            $table->timestamps();
+            // $table->timestamps();
         });
     }
 
