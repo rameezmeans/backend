a0a20c6801faafab5b66e93d57cf45e32931a142
Rameez Bhatti <xrkalix@gmail.com>
2024-12-04 12:20:25 +0500

    button there

--- database/migrations/2024_11_30_082324_create_file_event_table.php
+++ database/migrations/2024_11_30_082324_create_file_event_table.php
@@ -13,24 +13,24 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('file_event', function (Blueprint $table) {
+        // Schema::create('file_event', function (Blueprint $table) {
 
-            // $table->id();
+        //     // $table->id();
 
-            // $table->foreignId('temporary_file_id');
-            // $table->time('file_uploaded', precision: 0);
-            // $table->integer('credits_at_start');
+        //     // $table->foreignId('temporary_file_id');
+        //     // $table->time('file_uploaded', precision: 0);
+        //     // $table->integer('credits_at_start');
 
-            // $table->foreignId('stages')->default(0);
-            // $table->string('options')->nullable();
-            // $table->time('time_to_pick_stages_and_options', precision: 0)->nullable();
-            // $table->integer('credits_at_stages_and_options')->default(0);
-            // $table->integer('credits_for_stagees_and_options')->default(0);
+        //     // $table->foreignId('stages')->default(0);
+        //     // $table->string('options')->nullable();
+        //     // $table->time('time_to_pick_stages_and_options', precision: 0)->nullable();
+        //     // $table->integer('credits_at_stages_and_options')->default(0);
+        //     // $table->integer('credits_for_stagees_and_options')->default(0);
 
-            // $tab
+        //     // $tab
 
-            // $table->timestamps();
-        });
+        //     // $table->timestamps();
+        // });
     }
 
     /**
@@ -40,6 +40,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('file_event');
+        // Schema::dropIfExists('file_event');
     }
 };
