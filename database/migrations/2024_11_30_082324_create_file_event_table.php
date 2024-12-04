6e84d3c9a0e1f2cf1ea5b1daff02d5fa63238903
Rameez Bhatti <xrkalix@gmail.com>
2024-12-04 12:01:01 +0500

    here we are

---
+++ database/migrations/2024_11_30_082324_create_file_event_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('file_event', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->foreignId('temporary_file_id');
+            $table->time('file_uploaded', precision: 0);
+            $table->integer('credits_at_start');
+
+            $table->foreignId('stages')->default(0);
+            $table->string('options')->nullable();
+            $table->time('time_to_pick_stages_and_options', precision: 0)->nullable();
+            $table->integer('credits_at_stages_and_options')->default(0);
+            $table->integer('credits_for_stagees_and_options')->default(0);
+
+            $tab
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('file_event');
+    }
+};
