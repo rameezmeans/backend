c5a569f8acfdbbbfd665756c2a7c5879739552d1
Rameez Bhatti <xrkalix@gmail.com>
2024-12-05 09:06:56 +0500

    button there

--- resources/views/files/show.blade.php
+++ resources/views/files/show.blade.php
@@ -518,7 +518,8 @@ margin-bottom: 10px !important;
                           </div>
                           <div class="clearfix"></div>
                         </div>
-        
+                        
+                        @if($file->ecu)
                         <div class="b-b b-grey p-l-20 p-r-20 p-b-10 p-t-10">
                           <p class="pull-left">ECU</p>
                           <div class="pull-right">
@@ -530,7 +531,8 @@ margin-bottom: 10px !important;
                           </div>
                           <div class="clearfix"></div>
                         </div>
-								  
+                        @endif
+
 								  @if($file->gearbox_ecu)
                         <div class="b-b b-grey p-l-20 p-r-20 p-b-10 p-t-10">
                           <p class="pull-left">Gearbox ECu</p>
@@ -3022,7 +3024,7 @@ margin-bottom: 10px !important;
                           <div class="clearfix"></div>
                         </div>
         
-                        
+                        @if($file->ecu)
                         <div class="b-b b-grey p-l-20 p-r-20 p-b-10 p-t-10">
                           <p class="pull-left">ECU</p>
                           <div class="pull-right">
@@ -3034,6 +3036,7 @@ margin-bottom: 10px !important;
                           </div>
                           <div class="clearfix"></div>
                         </div>
+                        @endif
                         
 						@if($file->gearbox_ecu)
                         <div class="b-b b-grey p-l-20 p-r-20 p-b-10 p-t-10">
@@ -3042,7 +3045,7 @@ margin-bottom: 10px !important;
                             @if($file->gearbox_ecu)
                               <span class="label bg-warning">{{App\Models\ECU::findOrFail($file->gearbox_ecu)->type}}<span>
                             @else
-                              <span class="label label-danger">NO ECU<span>
+                              <span class="label label-danger">NO Gear Box ECU<span>
                             @endif
                           </div>
                           <div class="clearfix"></div>
