eff915624581be40454b01a9f63e81282cf0c923
Rameez Bhatti <xrkalix@gmail.com>
2024-12-08 13:05:21 +0500

    dtc thing

--- resources/views/dtc_lookup/index.blade.php
+++ resources/views/dtc_lookup/index.blade.php
@@ -24,7 +24,7 @@
             <div class="card-body">
                 <div id="tableWithSearch_wrapper" class="dataTables_wrapper no-footer m-t-40">
                     <div>
-                        <table class="table table-hover demo-table-search table-responsive-block dataTable no-footer" id="tableWithSearch" role="grid" aria-describedby="tableWithSearch_info">
+                        <table class="table table-hover demo-table-search table-responsive-block no-footer" id="tableWithSearch" role="grid" aria-describedby="tableWithSearch_info">
                             <thead>
                                 <tr role="row">
                                     <th class="" tabindex="0" aria-controls="tableWithSearch" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Title: activate to sort column descending">Code</th>
