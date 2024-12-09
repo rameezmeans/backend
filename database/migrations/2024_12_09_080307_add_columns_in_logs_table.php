1667d341f03e49078ba5765d1b739dca8f8da566
Rameez Bhatti <xrkalix@gmail.com>
2024-12-09 11:07:51 +0500

    call response thing

---
+++ database/migrations/2024_12_09_080307_add_columns_in_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('logs', function (Blueprint $table) {
+            $table->foreignId('credit_id')->default(0);
+            $table->longText('call')->nullable();
+            $table->longText('response')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('logs', function (Blueprint $table) {
+            //
+        });
+    }
+};
