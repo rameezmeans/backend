dedf0ea9aa24086a9372b3f3b6605a35df15433d
Rameez Bhatti <xrkalix@gmail.com>
2024-12-18 15:48:11 +0500

    acm file renaming

--- resources/views/logs/zoho_logs.blade.php
+++ resources/views/logs/zoho_logs.blade.php
@@ -54,9 +54,7 @@
                                         <td class="v-align-middle semi-bold sorting_1">
                                             <p><a class="btn btn-warning text-black" target="_blank" href="{{route('zoho-details', $log->id)}}">Details</a></p>
                                         </td>
-                                        <td class="v-align-middle semi-bold sorting_1">
-                                            <p><a class="btn btn-warning text-black" target="_blank" href="{{route('zoho-details', $log->id)}}">Details</a></p>
-                                        </td>
+                                        
                                     </tr>
                                     @endif
                                 @endforeach
