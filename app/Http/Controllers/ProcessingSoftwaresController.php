d5dfddd33458eb82bdeb5b131f0f2982a8b59fd8
Rameez Bhatti <xrkalix@gmail.com>
2024-12-23 18:12:46 +0500

    file thing

--- app/Http/Controllers/ProcessingSoftwaresController.php
+++ app/Http/Controllers/ProcessingSoftwaresController.php
@@ -74,6 +74,8 @@ class ProcessingSoftwaresController extends Controller
 
     public function getExternalSourced(Request $request){
 
+        $button = false;
+
         $softwaresAndBrandsRecords = File::join('file_reply_software_service', 'file_reply_software_service.file_id', '=', 'files.id')
         ->whereNotNull('files.ecu')
         ->select('files.brand as brand', 'files.model as model', 'files.version as version', 'files.engine as engine', 'files.ecu as ecu', 'files.id as file_id', 'file_reply_software_service.service_id as service_id','file_reply_software_service.software_id as software_id')
@@ -82,11 +84,13 @@ class ProcessingSoftwaresController extends Controller
         ->distinct('service_id')
         ->get();
         
-        return view('processing_softwares.database_import', ['softwaresAndBrandsRecords' => $softwaresAndBrandsRecords]);
+        return view('processing_softwares.database_import', ['softwaresAndBrandsRecords' => $softwaresAndBrandsRecords, 'button' => $button]);
 
     }
 
     public function databaseImport(){
+
+        $button = true;
         
         $softwaresAndBrandsRecords = File::join('file_reply_software_service', 'file_reply_software_service.file_id', '=', 'files.id')
         ->whereNotNull('files.ecu')
@@ -94,7 +98,7 @@ class ProcessingSoftwaresController extends Controller
         ->distinct('service_id')
         ->get();
         
-        return view('processing_softwares.database_import', ['softwaresAndBrandsRecords' => $softwaresAndBrandsRecords]);
+        return view('processing_softwares.database_import', ['softwaresAndBrandsRecords' => $softwaresAndBrandsRecords, 'button' => $button]);
     }
 
     public function softwareReport(){
