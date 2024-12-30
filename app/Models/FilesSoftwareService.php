c005e5d81599c5dfea93addbedfd0d591b26cab2
Rameez Bhatti <xrkalix@gmail.com>
2024-12-30 18:28:00 +0500

    chagne status thing

---
+++ app/Models/FilesSoftwareService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FilesSoftwareService extends Model
+{
+    protected $table = "files_software_service_database";
+    use HasFactory;
+}
