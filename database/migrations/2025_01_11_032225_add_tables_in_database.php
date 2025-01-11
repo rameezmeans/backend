b07dfd233f8743bc3b637933cb92372dc2c1d9a3
Rameez Bhatti <xrkalix@gmail.com>
2025-01-11 06:31:24 +0500

    tools thing

--- database/migrations/2025_01_11_032225_add_tables_in_database.php
+++ database/migrations/2025_01_11_032225_add_tables_in_database.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('modifications', function (Blueprint $table) {
+        Schema::create('modifications', function (Blueprint $table) {
             $table->string('name');
         });
 
