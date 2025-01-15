f24386db5d5b0c600a37905bc50f8bfb5bfdfa5e
Rameez Bhatti <xrkalix@gmail.com>
2025-01-15 18:17:49 +0500

    timer thing

--- app/Http/Controllers/ProcessingSoftwaresController.php
+++ app/Http/Controllers/ProcessingSoftwaresController.php
@@ -14,7 +14,7 @@ class ProcessingSoftwaresController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-        $this->middleware('adminOnly');
+        // $this->middleware('adminOnly');
     }
 
     /**
