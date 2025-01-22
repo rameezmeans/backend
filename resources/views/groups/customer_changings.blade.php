0ef6da8637baf1423d74478ffb3f3af74aa24eeb
Rameez Bhatti <xrkalix@gmail.com>
2025-01-22 19:57:18 +0500

    cols in credits

--- resources/views/groups/customer_changings.blade.php
+++ resources/views/groups/customer_changings.blade.php
@@ -14,7 +14,7 @@
                 </div>
                 <div class="pull-right">
                 <div class="col-xs-12">
-                    <button data-redirect="{{ route('changes', $userId) }}" class="btn btn-success btn-cons m-b-10 redirect-click" type="button"><i class="pg-plus_circle"></i> <span class="bold">Create Tool</span>
+                    <button data-redirect="{{ route('changes', $user->id) }}" class="btn btn-success btn-cons m-b-10 redirect-click" type="button"><i class="pg-plus_circle"></i> <span class="bold">Create Tool</span>
                     </button>
                     {{-- <input type="text" id="search-table" class="form-control pull-right" placeholder="Search"> --}}
                 </div>
