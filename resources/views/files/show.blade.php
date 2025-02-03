a575c08b3d22cff7a67d5c60b9a57bc17dbd0fe9
Rameez Bhatti <xrkalix@gmail.com>
2025-02-03 16:17:38 +0500

    question form

--- resources/views/files/show.blade.php
+++ resources/views/files/show.blade.php
@@ -6714,6 +6714,7 @@ re_calculate_proposed_credits(file_id);
 $(document).on('click', '.btn-add-message', function(e){
     console.log('oh yes');
 
+    let file_id = $(this).data('file_id');
     let formElements = $("#addMessageForm-"+file_id).serializeArray();
 
     console.log(formElements);
