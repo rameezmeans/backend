8df64d44f4fe3ab134be7907b890c01d519e00db
Rameez Bhatti <xrkalix@gmail.com>
2025-02-14 17:25:06 +0500

    invoices thing

--- resources/views/files/show.blade.php
+++ resources/views/files/show.blade.php
@@ -3389,11 +3389,14 @@ margin-bottom: 10px !important;
                         @endif
 
                         
-                          <div class="text-center m-t-20">  
-                            @if($file->ecu)                  
-                              <a class="btn btn-success btn-cons m-b-10" href="{{route('add-comments', [$vehicle->id, 'file='.$file->id])}}"><span class="bold">Go To Comments</span></a>
-                            @endif
+                          <div class="text-center m-t-20">
+                            @if($vehicle)
+                              @if($file->ecu)                  
+                                <a class="btn btn-success btn-cons m-b-10" href="{{route('add-comments', [$vehicle->id, 'file='.$file->id])}}"><span class="bold">Go To Comments</span></a>
+                              @endif
+                            
                             <a class="btn btn-success btn-cons m-b-10" href="{{route('vehicle', $vehicle->id)}}"><span class="bold">Go To Vehicle</span></a>
+                            @endif
                             <a class="btn btn-success btn-cons m-b-10" href="{{route('edit-file', $file->id)}}"><span class="bold">Edit File</span></a>
                             
                               
