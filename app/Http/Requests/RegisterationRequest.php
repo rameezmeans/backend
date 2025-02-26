6ea85435c616a8652eb2f9f473574e0268542078
Rameez Bhatti <xrkalix@gmail.com>
2025-02-26 10:47:25 +0500

    registeration

---
+++ app/Http/Requests/RegisterationRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'front_end_id' => ['required'],
+            'name' => ['required', 'string', 'max:255'],
+            'phone' => ['required', 'string', 'max:255'],
+            'language' => ['required', 'string', 'max:255'],
+            'address' => ['required', 'string', 'max:255'],
+            'zip' => ['required', 'string', 'max:255'],
+            'city' => ['required', 'string', 'max:255'],
+            'country' => ['required', 'string', 'max:255'],
+            'status' => ['required', 'string', 'max:255'],
+            'company_name' => ['max:255'],
+            'company_id' => ['max:255'],
+            'slave_tools_flag' => ['string', 'max:255'],
+            'master_tools' => [],
+            'slave_tools' => [],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ];
+    }
+}
