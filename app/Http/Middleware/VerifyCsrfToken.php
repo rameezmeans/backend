b8efcc2ebaeb0af9f26677f95aef33311f75ceda
Rameez Bhatti <xrkalix@gmail.com>
2025-03-07 00:26:37 +0500

    csrf token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'makelua'
+        'makelua', 'api/*', // Exclude all API routes from CSRF protection
     ];
 }
