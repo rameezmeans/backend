2ba74e7c8a1d74ef18eb44b8db98378be430e381
Rameez Bhatti <xrkalix@gmail.com>
2025-04-12 11:07:15 +0500

    config thing

--- app/Http/Livewire/NewPaymentLog.php
+++ app/Http/Livewire/NewPaymentLog.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Credit;
 use Livewire\Component;
+
+use Illuminate\Database\Eloquent\Builder;
+
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Contracts\View\View;
 
 class NewPaymentLog extends Component
 {
     public function render()
     {
         return view('livewire.new-payment-log');
+    }
+
+    use Tables\Concerns\InteractsWithTable;
+
+    protected function getTableQuery(): Builder
+    {
+        return Credit::query()->where('credits', '>', 0)->where('price_payed', '>', 0);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id'),
+                
+            ]);
     }
 }
