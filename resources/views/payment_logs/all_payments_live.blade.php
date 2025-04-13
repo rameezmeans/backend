6ddf0783328cc4f854f409787a77bb7ef3432db6
Rameez Bhatti <xrkalix@gmail.com>
2025-04-13 06:48:36 +0500

    publish assets

--- resources/views/payment_logs/all_payments_live.blade.php
+++ resources/views/payment_logs/all_payments_live.blade.php
@@ -4,6 +4,8 @@
 <script src="https://cdn.tailwindcss.com"></script>
 <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
