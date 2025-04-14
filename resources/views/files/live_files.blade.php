8a91f68f0cae28e6c59eec4cff0b0cec1be19025
Rameez Bhatti <xrkalix@gmail.com>
2025-04-14 15:11:54 +0500

    country thing in credits

--- resources/views/files/live_files.blade.php
+++ resources/views/files/live_files.blade.php
@@ -18,7 +18,7 @@
     background-color: transparent !important;
   }
 
-  .bg-gray-500 {
+  .bg-gray {
     --tw-bg-opacity: 1;
     background-color: rgb(107 114 128 / var(--tw-bg-opacity, 1)) !important;
 }
