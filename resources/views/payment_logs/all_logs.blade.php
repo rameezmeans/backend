1ce0e316e061b3c35a6443afb9c5b1b087c4302f
Rameez Bhatti <xrkalix@gmail.com>
2025-04-15 14:58:33 +0500

    country thing in credits

--- resources/views/payment_logs/all_logs.blade.php
+++ resources/views/payment_logs/all_logs.blade.php
@@ -27,12 +27,12 @@
                     <div>
                         <table class="table table-hover demo-table-search table-responsive-block data-table no-footer" id="tableWithSearch" role="grid" aria-describedby="tableWithSearch_info">
                             <thead>
-                                <tr role="row">
+                                <tr>
                                     
 
                                     <th>DB ID</th>
                                     <th>Invoice ID</th>
-                                    <th>Date</th>
+                                    {{-- <th>Date</th>
                                     <th>Customer</th>
                                     <th>Email</th>
                                     <th>Customer Group</th>
@@ -41,84 +41,15 @@
                                     <th>Price Payed (€)</th>
                                     <th>Elorus</th>
                                     <th>Zohobooks</th>
-                                    <th>Trouble Email Sent</th>
+                                    <th>Trouble Email Sent</th> --}}
                                     
                                 </tr>
                             </thead>
                             <tbody>
-                                {{-- @if($allPaymentLogs)
-                                    @foreach ($allPaymentLogs as $l)
-
-                                        @if(\App\Models\Credit::where('id', $l->payment_id)->first())
-                                        <tr role="row" class="">
-
-                                            <td class="v-align-middle semi-bold sorting_1">
-                                                <p class="label @if(\App\Models\Credit::findOrFail($l->payment_id)->front_end_id == 2) bg-warning @else bg-primary text-white @endif">{{\App\Models\Credit::findOrFail($l->payment_id)->id}}</p>
-                                            </td>
-
-                                            <td class="v-align-middle semi-bold sorting_1">
-                                                <p class="label @if(\App\Models\Credit::findOrFail($l->payment_id)->front_end_id == 2) bg-warning @else bg-primary text-white @endif">{{\App\Models\Credit::findOrFail($l->payment_id)->invoice_id}}</p>
-                                            </td>
-
-                                            <td class="v-align-middle semi-bold sorting_1">
-                                                <p>{{date('d/m/Y',strtotime($l->created_at))}}</p>
-                                                
-                                            </td>
-
-                                            <td class="v-align-middle semi-bold sorting_1">
-                                                <p>{{\App\Models\User::findOrFail($l->user_id)->name}}</p>
-                                            </td>
-
-                                            <td class="v-align-middle semi-bold sorting_1">
-                                                <p>{{\App\Models\User::findOrFail($l->user_id)->email}}</p>
-                                            </td>
-
-                                            <td class="v-align-middle semi-bold sorting_1">
-                                                @if(\App\Models\User::findOrFail($l->user_id)->group != NULL)
-                                                    <p>{{\App\Models\User::findOrFail($l->user_id)->group->name}}</p>
-                                                @else
-                                                    <p>No Group</p>
-                                                @endif
-                                            </td>
-
-                                            <td class="v-align-middle semi-bold sorting_1">
-                                                <p>{{\App\Models\Credit::findOrFail($l->payment_id)->invoice_id}}</p>
-                                            </td>
-                                            
-                                            <td class="v-align-middle semi-bold sorting_1">
-                                                <p>{{$l->elorus_invoice_id}}</p>
-                                            </td>
-
-                                            <td class="v-align-middle semi-bold sorting_1">
-                                                <p>{{\App\Models\Credit::findOrFail($l->payment_id)->credits}}</p>
-                                            </td>
-
-                                            <td class="v-align-middle semi-bold sorting_1">
-                                                <p>${{\App\Models\Credit::findOrFail($l->payment_id)->price_payed}}</p>
-                                            </td>
-
-                                            <td class="v-align-middle semi-bold sorting_1">
-                                                <p>@if($l->elorus_id)<a class="btn btn-warning text-black" target="_blank" href="{{\App\Models\Credit::findOrFail($l->payment_id)->elorus_permalink}}">To Elorus</a>@else No Elorus @endif</p>
-                                                
-                                            </td>
-
-                                            <td class="v-align-middle semi-bold sorting_1">
-                                                <p>@if($l->zohobooks_id)<a class="btn btn-warning text-black" target="_blank" href="{{'https://books.zoho.com/app/8745725#/invoices/'.$l->zohobooks_id}}">To Zohobooks</a>@else No Zohobooks @endif</p>
-                                                
-                                            </td>
-
-                                            <td class="v-align-middle semi-bold sorting_1">
-                                                <p>@if($l->email_sent) Yes @else No @endif</p>
-                                                
-                                            </td>
-
-                                            
-                                        </tr>
-                                        @endif
-                                    @endforeach
-                                @endif --}}
+                                
                             </tbody>
                         </table>
+
                     </div>
                 </div>
             </div>
@@ -139,7 +70,7 @@
 
             processing: true,
             serverSide: true,
-            order: [[0,'desc']],
+            
             ajax: {
                 url: "{{ route('payment-logs-table') }}",
                 type: 'POST',
@@ -150,23 +81,23 @@
             columns: [
                 {data: 'db_id', name: 'db_id'},
                 {data: 'invoice_id', name: 'invoice_id'},
-                {
-                    data: 'created_at',
-                    type: 'num',
-                    render: {
-                        _: 'display',
-                        sort: 'timestamp'
-                    }
-                },
-                {data: 'customer', name: 'customer'},
-                {data: 'email', name: 'email'},
-                {data: 'group', name: 'group'},
-                {data: 'elorus_invoice_id', name: 'elorus_invoice_id'},
-                {data: 'credits', name: 'credits'},
-                {data: 'price_payed', name: 'price_payed'},
-                {data: 'elorus', name: 'elorus'},
-                {data: 'zohobooks', name: 'zohobooks'},
-                {data: 'email_sent', name: 'email_sent'},
+                // {
+                //     data: 'created_at',
+                //     type: 'num',
+                //     render: {
+                //         _: 'display',
+                //         sort: 'timestamp'
+                //     }
+                // },
+                // {data: 'customer', name: 'customer'},
+                // {data: 'email', name: 'email'},
+                // {data: 'group', name: 'group'},
+                // {data: 'elorus_invoice_id', name: 'elorus_invoice_id'},
+                // {data: 'credits', name: 'credits'},
+                // {data: 'price_payed', name: 'price_payed'},
+                // {data: 'elorus', name: 'elorus'},
+                // {data: 'zohobooks', name: 'zohobooks'},
+                // {data: 'email_sent', name: 'email_sent'},
                 
                 
             ]
