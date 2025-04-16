cd77893d5dc7477009f7ef55eae58ee880b226ed
Rameez Bhatti <xrkalix@gmail.com>
2025-04-16 09:51:29 +0500

    country thing in credits

--- resources/views/files/report-engineers-live.blade.php
+++ resources/views/files/report-engineers-live.blade.php
@@ -233,8 +233,10 @@
               
           ],
 
-          "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-          }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+          "buttons": ["csv", "excel"]
+          
+
+      });
 
       $(".filter").click(function(){
         table.draw();
