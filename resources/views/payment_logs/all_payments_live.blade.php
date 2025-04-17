e371bda41cf54da0da0b5a49113479d10e859359
Rameez Bhatti <xrkalix@gmail.com>
2025-04-17 11:14:24 +0500

    country thing in credits

--- resources/views/payment_logs/all_payments_live.blade.php
+++ resources/views/payment_logs/all_payments_live.blade.php
@@ -65,6 +65,70 @@
                 </div>
                 <div class="clearfix"></div>
 
+                <div class="card-group horizontal" id="accordion" role="tablist" aria-multiselectable="true">
+                  <div class="card card-default m-b-0">
+                    <div class="card-header " role="tab" id="headingOne">
+                      <h4 class="card-title">
+                          <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                           Collapsible Group Item
+                          </a>
+                        </h4>
+                    </div>
+                    <div id="collapseOne" class="collapse show" role="tabcard" aria-labelledby="headingOne">
+                      <div class="card-body">
+                        Click headers to expand/collapse content that is broken into logical sections, much like tabs. Optionally, toggle sections open/closed on mouseover.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card card-default m-b-0">
+                    <div class="card-header " role="tab" id="headingTwo">
+                      <h4 class="card-title">
+                          <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                          Typography Variables
+                          </a>
+                        </h4>
+                    </div>
+                    <div id="collapseTwo" class="collapse" role="tabcard" aria-labelledby="headingTwo">
+                      <div class="card-body">
+                        <h1 class="light">
+                          go explore the <span class="semi-bold">world</span>
+                        </h1>
+                        <h4>
+                          small things in life matters the most
+                        </h4>
+                        <h2>
+                          Big Heading <span class="semi-bold">Body</span>,
+                          <i>Variations</i>
+                        </h2>
+                        <h4>
+                          <span class="semi-bold">Open Me</span>, Light , <span class=
+                          "semi-bold">Bold</span>, <i>Everything</i>
+                        </h4>
+                        <p>
+                          is the art and technique of arranging type in order to make language visible. The arrangement of type involves the selection of typefaces, point size, line length, leading (line spacing), adjusting the spaces between groups of letters (tracking)
+                        </p>
+                        <p>
+                          and adjusting the Case space between pairs of letters (kerning). Type design is a closely related craft, which some consider distinct and others a part of typography
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card card-default m-b-0">
+                    <div class="card-header " role="tab" id="headingThree">
+                      <h4 class="card-title">
+                          <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Easy Edit
+                          </a>
+                        </h4>
+                    </div>
+                    <div id="collapseThree" class="collapse" role="tabcard" aria-labelledby="headingThree">
+                      <div class="card-body">
+                        Click headers to expand/collapse content that is broken into logical sections, much like tabs. Optionally, toggle sections open/closed on mouseover.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
                 
                 <div class="row m-t-20 m-b-20">
                   <div class="col-md-6">
