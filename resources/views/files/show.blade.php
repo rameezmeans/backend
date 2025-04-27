1b6a67d55ae817698e15e47e51d2450c6a7c678d
Rameez Bhatti <xrkalix@gmail.com>
2025-04-27 18:17:11 +0500

    flag thing

--- resources/views/files/show.blade.php
+++ resources/views/files/show.blade.php
@@ -1348,6 +1348,18 @@ margin-bottom: 10px !important;
 
                             @foreach($file->files->toArray() as $message)
 
+                            <div class="card-group horizontal" id="accordion" role="tablist" aria-multiselectable="true">
+                              <div class="card card-default m-b-0">
+                                <div class="card-header " role="tab" id="headingOne">
+                                  <h4 class="card-title">
+                                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                       Data Filters
+                                      </a>
+                                    </h4>
+                                </div>
+                                <div id="collapseOne" class="collapse show" role="tabcard" aria-labelledby="headingOne">
+                                  <div class="card-body">
+
                             <div class="card">
 
                               <!-- Nav tabs -->
@@ -1793,6 +1805,15 @@ margin-bottom: 10px !important;
                     
                     </div>
                             </div>
+
+
+                          </div>
+                        </div>
+                      </div>
+                      
+                      
+                    </div>
+
                       @endforeach
                       </div>
 
