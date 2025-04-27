74c8a8c20cb9bf4bd1269f60cc450b26ae6e3ff1
Rameez Bhatti <xrkalix@gmail.com>
2025-04-27 15:57:32 +0500

    flag thing

--- resources/views/files/show.blade.php
+++ resources/views/files/show.blade.php
@@ -5895,51 +5895,41 @@ margin-bottom: 10px !important;
         <div class="modal-header clearfix text-left">
           <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="pg-close fs-14"></i>
           </button>
-          <h5>Payment <span class="semi-bold">Information</span></h5>
-          <p class="p-b-10">We need payment information inorder to process your order</p>
+          <h5>Customer <span class="semi-bold">Comments</span></h5>
+          <p class="p-b-10"></p>
         </div>
         <div class="modal-body">
           <form role="form">
             <div class="form-group-attached">
               <div class="row">
                 <div class="col-md-12">
-                  <div class="form-group form-group-default">
-                    <label>Company Name</label>
-                    <input type="email" class="form-control">
+                  <div class="form-group form-group-default form-group-default-select2">
+                    <label>Comment Type</label>
+                    <select class=" full-width">
+                      <option value="bad">Bad Comment</option>
+                      <option value="good">Good Comment</option>
+                    </select>
                   </div>
                 </div>
               </div>
               <div class="row">
-                <div class="col-md-8">
+                <div class="col-md-12">
                   <div class="form-group form-group-default">
-                    <label>Card Number</label>
-                    <input type="text" class="form-control">
+                    <label>Comment</label>
+                    <textarea class="form-control"></textarea>
                   </div>
                 </div>
-                <div class="col-md-4">
-                  <div class="form-group form-group-default">
-                    <label>Card Holder</label>
-                    <input type="text" class="form-control">
-                  </div>
-                </div>
+                
               </div>
             </div>
-          </form>
+          
           <div class="row">
-            <div class="col-md-8">
-              <div class="p-t-20 clearfix p-l-10 p-r-10">
-                <div class="pull-left">
-                  <p class="bold font-montserrat text-uppercase">TOTAL</p>
-                </div>
-                <div class="pull-right">
-                  <p class="bold font-montserrat text-uppercase">$20.00</p>
-                </div>
-              </div>
-            </div>
+            
             <div class="col-md-4 m-t-10 sm-m-t-10">
-              <button type="button" class="btn btn-primary btn-block m-t-5">Pay Now</button>
+              <button type="button" class="btn btn-success btn-block m-t-5">Add Comment</button>
             </div>
           </div>
+        </form>
         </div>
       </div>
     </div>
