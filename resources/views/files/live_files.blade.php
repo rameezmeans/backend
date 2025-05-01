d379f98d2012435c629ccbd2a700c9314c49b76d
Rameez Bhatti <xrkalix@gmail.com>
2025-05-02 00:06:57 +0500

    online offline thing

--- resources/views/files/live_files.blade.php
+++ resources/views/files/live_files.blade.php
@@ -67,7 +67,7 @@
 
                   <div>
                     @foreach($allEngineers as $engineer)
-                      <div class="card social-card share col1 @if($loggedInUser->id !== 3 && $loggedInUser->id == $engineer->id) flip-status @endif" data-social="item" style="">
+                      <div class="card social-card share col1 @if($loggedInUser->id !== 3 && $loggedInUser->id == $engineer->id) flip-status @endif" data-social="item" style="" data-id="{{$engineer->id}}">
                         <div class="circle" data-toggle="tooltip" title="" data-container="body" data-original-title="Label">
                         </div>
                         <div class="card-header clearfix">
@@ -476,6 +476,8 @@
         $('.parent-adjusted').parent().addClass('flex');
 
         $(document).on('click','.flip-status',function(e) {
+
+          let id = 
 
           $.ajax({
                 url: "/flip_engineer_status",
