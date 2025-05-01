774f595f95b70516a3f9b686402e09c052dfefcf
Rameez Bhatti <xrkalix@gmail.com>
2025-05-01 20:36:03 +0500

    online offline thing

--- resources/views/files/live_files.blade.php
+++ resources/views/files/live_files.blade.php
@@ -75,7 +75,11 @@
                             <img alt="Profile Image" width="33" height="33" data-src-retina="assets/img/profiles/4x.jpg" data-src="assets/img/profiles/4.jpg" src="assets/img/profiles/4x.jpg">
                           </div>
                           <h5>{{$engineer->name}}</h5>
+                          @if($engineer->online)
                           <h5 class="text-success pull-left fs-12">Online <i class="fa fa-circle text-success fs-11"></i></h5>
+                          @else
+                          <h5 class="text-danger pull-left fs-12">Offline <i class="fa fa-circle text-danger fs-11"></i></h5>
+                          @endif
                         </div>
                         
                       </div>
