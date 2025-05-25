0e69b10efbe8978d82c32521d73c3fd865bf73ae
Rameez Bhatti <xrkalix@gmail.com>
2025-05-26 02:55:49 +0500

    support_messages_engineer thing

--- resources/views/files/engineers_assignment.blade.php
+++ resources/views/files/engineers_assignment.blade.php
@@ -64,6 +64,7 @@
                         <select class="form-control" id="support_messages_engineer" name="support_messages_engineer">
                             
                           @foreach ($allEngineers as $engineer)
+                            <option @if($supportMessagesEngineer == -1) selected  @endif value="-1">Same Engineer who uploaded the last version</option>
                             <option @if($supportMessagesEngineer == $engineer->id) selected  @endif value="{{$engineer->id}}">{{$engineer->name}}</option>
                           @endforeach
   
