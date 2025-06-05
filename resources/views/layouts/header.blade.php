b86e36480c58cbf0e7e66ef3fad4cb08ef73fac5
Rameez Bhatti <xrkalix@gmail.com>
2025-06-05 19:59:56 +0500

    api thing

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -147,7 +147,7 @@
         <li class="p-r-10 inline">
           @foreach($allEngineers as $engineer)
                     
-                      <div class="card social-card share col1 @if($loggedInUser->id == $engineer->id) flip-status @endif" data-social="item" style="" data-id="{{$engineer->id}}">
+                      <div style="width: 50%;" class="card social-card share col1 @if($loggedInUser->id == $engineer->id) flip-status @endif" data-social="item" style="" data-id="{{$engineer->id}}">
                         <div class="circle" data-toggle="tooltip" title="" data-container="body" data-original-title="Label">
                         </div>
                         <div class="card-header clearfix">
