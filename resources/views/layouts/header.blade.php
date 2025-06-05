e116129892e11d48fc4aae4f1e99f00b8ab46ccd
Rameez Bhatti <xrkalix@gmail.com>
2025-06-05 20:02:14 +0500

    api thing

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -147,21 +147,21 @@
         <li class="p-r-10 inline">
           @foreach($allEngineers as $engineer)
                     
-                      <div style="width: 50%;" class="card social-card share col1 @if($loggedInUser->id == $engineer->id) flip-status @endif" data-social="item" style="" data-id="{{$engineer->id}}">
-                        <div class="circle" data-toggle="tooltip" title="" data-container="body" data-original-title="Label">
-                        </div>
-                        <div class="card-header clearfix">
-                          <div class="user-pic">
-                            <img alt="Profile Image" width="33" height="33" data-src-retina="{{url('assets/img/profiles/4x.jpg')}}" data-src="{{url('assets/img/profiles/4.jpg')}}" src="{{url('assets/img/profiles/4x.jpg')}}">
-                          </div>
-                          <h5>{{$engineer->name}}</h5>
-                            @if($engineer->online)
-                              <h5 class="text-success pull-left fs-12">Online <i class="fa fa-circle text-success fs-11"></i></h5>
-                            @else
-                              <h5 class="text-danger pull-left fs-12">Offline <i class="fa fa-circle text-danger fs-11"></i></h5>
-                            @endif
-                        </div>
-                      </div>
+                <div class="card social-card share col1 @if($loggedInUser->id == $engineer->id) flip-status @endif" data-social="item" style="" data-id="{{$engineer->id}}">
+                  <div class="circle" data-toggle="tooltip" title="" data-container="body" data-original-title="Label">
+                  </div>
+                  <div class="card-header clearfix">
+                    <div class="user-pic">
+                      <img alt="Profile Image" width="33" height="33" data-src-retina="{{url('assets/img/profiles/4x.jpg')}}" data-src="{{url('assets/img/profiles/4.jpg')}}" src="{{url('assets/img/profiles/4x.jpg')}}">
+                    </div>
+                    <h5>{{$engineer->name}}</h5>
+                      @if($engineer->online)
+                        <h5 class="text-success pull-left fs-12">Online <i class="fa fa-circle text-success fs-11"></i></h5>
+                      @else
+                        <h5 class="text-danger pull-left fs-12">Offline <i class="fa fa-circle text-danger fs-11"></i></h5>
+                      @endif
+                  </div>
+                </div>
                     
                     @endforeach
           {{-- <a href="#" class="header-icon pg pg-link"></a> --}}
