244ba33152ab8e8208a7f40c0ce965f64068734c
Rameez Bhatti <xrkalix@gmail.com>
2025-06-05 19:26:14 +0500

    api thing

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,6 +11,30 @@
       <!-- START NOTIFICATION LIST -->
       <ul class="d-lg-inline-block d-none notification-list no-margin d-lg-inline-block b-grey b-l b-r no-style p-l-30 p-r-20">
         <li class="p-r-10 inline">
+
+          @php
+          $loggedInUser = Auth::user();
+          $allEngineers = App\Models\User::whereIn('role_id', [2,3])->where('test', 0)->whereNull('subdealer_group_id')->orWhere('id', 3)->get();
+        @endphp
+        <div>
+                    @foreach($allEngineers as $engineer)
+                      <div style="width: 40%;" class="card social-card share col1 @if($loggedInUser->id == $engineer->id) flip-status @endif" data-social="item" style="" data-id="{{$engineer->id}}">
+                        <div class="circle" data-toggle="tooltip" title="" data-container="body" data-original-title="Label">
+                        </div>
+                        <div class="card-header clearfix">
+                          <div class="user-pic">
+                            <img alt="Profile Image" width="33" height="33" data-src-retina="{{url('assets/img/profiles/4x.jpg')}}" data-src="{{url('assets/img/profiles/4.jpg')}}" src="{{url('assets/img/profiles/4x.jpg')}}">
+                          </div>
+                          <h5>{{$engineer->name}}</h5>
+                            @if($engineer->online)
+                              <h5 class="text-success pull-left fs-12">Online <i class="fa fa-circle text-success fs-11"></i></h5>
+                            @else
+                              <h5 class="text-danger pull-left fs-12">Offline <i class="fa fa-circle text-danger fs-11"></i></h5>
+                            @endif
+                        </div>
+                      </div>
+                    @endforeach
+                  </div>
           {{-- <div class="dropdown">
             <a href="javascript:;" id="notification-center" class="header-icon pg pg-world" data-toggle="dropdown">
               <span class="bubble"></span>
@@ -139,29 +163,7 @@
     <div class="d-flex align-items-center">
 
       <div class="pull-right p-r-10 fs-14 font-heading d-lg-block d-none">
-        @php
-          $loggedInUser = Auth::user();
-          $allEngineers = App\Models\User::whereIn('role_id', [2,3])->where('test', 0)->whereNull('subdealer_group_id')->orWhere('id', 3)->get();
-        @endphp
-        <div>
-                    @foreach($allEngineers as $engineer)
-                      <div style="width: 20%;" class="card social-card share col1 @if($loggedInUser->id == $engineer->id) flip-status @endif" data-social="item" style="" data-id="{{$engineer->id}}">
-                        <div class="circle" data-toggle="tooltip" title="" data-container="body" data-original-title="Label">
-                        </div>
-                        <div class="card-header clearfix">
-                          <div class="user-pic">
-                            <img alt="Profile Image" width="33" height="33" data-src-retina="{{url('assets/img/profiles/4x.jpg')}}" data-src="{{url('assets/img/profiles/4.jpg')}}" src="{{url('assets/img/profiles/4x.jpg')}}">
-                          </div>
-                          <h5>{{$engineer->name}}</h5>
-                            @if($engineer->online)
-                              <h5 class="text-success pull-left fs-12">Online <i class="fa fa-circle text-success fs-11"></i></h5>
-                            @else
-                              <h5 class="text-danger pull-left fs-12">Offline <i class="fa fa-circle text-danger fs-11"></i></h5>
-                            @endif
-                        </div>
-                      </div>
-                    @endforeach
-                  </div>
+        
       </div>
       <!-- START User Info-->
       <div class="pull-left p-r-10 fs-14 font-heading d-lg-block d-none">
