552abb2050f1bd8b40d8a1a5c3851c633b225f0a
Rameez Bhatti <xrkalix@gmail.com>
2025-07-01 01:42:17 +0500

    auto search file

---
+++ database/migrations/2025_04_29_124516_add_user_id_in_file_logger_table copy.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('auto_file_info', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('temporary_file_id');
+            $table->foreignId('auto_searched_file_id');
+            $table->string('brand');
+            $table->string('model');
+            $table->string('version');
+            $table->string('engine');
+            $table->boolean('is_modified')->default(0);
+            $table->string('modification');
+            $table->string('gearbox');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
--- database/migrations/2025_04_29_124516_add_user_id_in_file_logger_table.php
+++ database/migrations/2025_04_29_124516_add_user_id_in_file_logger_table.php
@@ -13,18 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('auto_file_info', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('temporary_file_id');
-            $table->foreignId('auto_searched_file_id');
-            $table->string('brand');
-            $table->string('model');
-            $table->string('version');
-            $table->string('engine');
-            $table->boolean('is_modified')->default(0);
-            $table->string('modification');
-            $table->string('gearbox');
-            $table->timestamps();
+        Schema::table('file_logger', function (Blueprint $table) {
+            $table->foreignId('user_id');
         });
     }
 
@@ -38,3 +28,4 @@ return new class extends Migration
         //
     }
 };
+
