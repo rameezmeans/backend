8865ab55b82018911c9a31a0df94a376c1063e3c
Rameez Bhatti <xrkalix@gmail.com>
2025-07-01 01:36:35 +0500

    auto search file

--- database/migrations/2025_04_29_124516_add_user_id_in_file_logger_table.php
+++ database/migrations/2025_04_29_124516_add_user_id_in_file_logger_table.php
@@ -13,8 +13,18 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('file_logger', function (Blueprint $table) {
-            $table->foreignId('user_id');
+        Schema::table('auto_file_info', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('temporary_file_id');
+            $table->foreignId('auto_searched_file_id');
+            $table->string('brand');
+            $table->string('model');
+            $table->string('version');
+            $table->string('engine');
+            $table->boolean('is_modified')->default(0);
+            $table->string('modification');
+            $table->string('gearbox');
+            $table->timestamps();
         });
     }
 
