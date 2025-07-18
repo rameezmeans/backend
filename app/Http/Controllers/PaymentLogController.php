59fd80955781dfd85fb61e169b255d4cbc115348
Rameez Bhatti <xrkalix@gmail.com>
2025-07-19 04:29:20 +0500

    email thing

--- app/Http/Controllers/PaymentLogController.php
+++ app/Http/Controllers/PaymentLogController.php
@@ -98,7 +98,8 @@ class PaymentLogController extends Controller
 
             if ($request->filled('from_date') && $request->filled('to_date')) {
 
-                $data = $data->whereBetween('created_at', [$request->from_date, $request->to_date]);
+                $data = $data->whereDate('created_at', '>=', $request->from_date)
+                ->whereDate('created_at', '<=', $request->to_date);
 
             }
 
